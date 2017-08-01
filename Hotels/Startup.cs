449c4be71f54fe997bd811c4950dea1b9025ea14
biancaPuscasu <biancapuscasu@yahoo.com>
2017-08-01 10:27:25 +0300

    Created solution

---
+++ Hotels/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hotels.Startup))]
+namespace Hotels
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
