a533ea230b2f29c1747e1a85a22695085788cde3
oanabrisc <oana.brisc29@gmail.com>
2017-08-03 10:43:26 +0300

    fixed errors

--- DataLayer/Migrations/Configuration.cs
+++ DataLayer/Migrations/Configuration.cs
@@ -60,13 +60,13 @@ namespace DataLayer.Migrations
 			);
 
 
-			
+
 
 			context.Reservations.AddOrUpdate(
-				new Reservation { Id = 1, DateOfReservation =new DateTime( 2017,04,01), DateOfStart = new DateTime (2017,04,10),
-				     DateOfEnd=new DateTime(2017,04,15),TotalPayment = 400,NumberOfPeople = 3,RoomReservationId = 1,UserId = 1}
-				new Reservation { Id = 2, DateOfReservation =new DateTime( 2017,05,02), DateOfStart = new DateTime (2017,05,11),
-				     DateOfEnd=new DateTime(2017,05,13),TotalPayment = 300,NumberOfPeople = 2,RoomReservationId = 2,UserId = 2},
+				new Reservation { Id = 1, DateOfReservation = new DateTime(2017, 04, 01), DateOfStart = new DateTime(2017, 04, 10),
+					DateOfEnd = new DateTime(2017, 04, 15), TotalPayment = 400, NumberOfPeople = 3, RoomReservationId = 1, UserId = 1 },
+			new Reservation { Id = 2, DateOfReservation = new DateTime(2017, 05, 02), DateOfStart = new DateTime(2017, 05, 11),
+				DateOfEnd = new DateTime(2017, 05, 13), TotalPayment = 300, NumberOfPeople = 2, RoomReservationId = 2, UserId = 2 },
 				new Reservation{Id = 3,DateOfReservation = new DateTime(2017,06,07),DateOfStart = new DateTime(2017,06,20),
 					DateOfEnd = new DateTime(2017,06,25),TotalPayment = 500,NumberOfPeople = 2,RoomReservationId = 3,UserId =1 }
 			);
