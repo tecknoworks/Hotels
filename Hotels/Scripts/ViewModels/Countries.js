32bde3707c8427aa070fa6ea999e6f7738fbf271
oanabrisc <oana.brisc29@gmail.com>
2017-08-09 10:40:46 +0300

    shows acomodations

--- Hotels/Scripts/ViewModels/Countries.js
+++ Hotels/Scripts/ViewModels/Countries.js
@@ -2,12 +2,27 @@
     var self = this;
     self.Countries = ko.observableArray();
     self.Cities = ko.observableArray();
+   
+    self.Acomodations = ko.observableArray();
     self.Id = ko.observable();
+    self.Type = ko.observable();
+    self.Address = ko.observable();
     self.Name = ko.observable();
+    self.NumberOfStars = ko.observable();
+    self.Description = ko.observable();
+    self.PhoneNumber = ko.observable();
+    self.WebSite = ko.observable();
 
     self.details = function (data) {
-        self.Id(data.Id);
-        self.Name(data.Name);
+    	self.Id(data.Id);
+    	self.Type(data.Type);
+    	self.Address(data.Address);
+    	self.Name(data.Name);
+    	self.NumberOfStars(data.NumberOfStars);
+    	self.Description(data.Description);
+    	self.PhoneNumber(data.PhoneNumber);
+    	self.WebSite(data.WebSite);
+
     };
 
     self.refresh = function () {
@@ -38,5 +53,20 @@
     			console.log(textStatus + ': ' + errorThrown);
     		}
     	});
+    	self.getAcomodations = function (data) {
+    		var url = '/Home/GetAcomodations';
+    		$.ajax(url, {
+    			data: { cityId: data.Id },
+    			type: "get",
+    			contentType: "application/json; charset=utf-8",
+    			success: function (data) {
+    				self.Acomodations(data.Acomodations);
+    				$("#acomodations").show();
+    			},
+    			error: function (jqXHR, textStatus, errorThrown) {
+    				console.log(textStatus + ': ' + errorThrown);
+    			}
+    		});
+    	}
     }
 }
