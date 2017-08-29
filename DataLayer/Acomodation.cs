075747d1331ec596fe738df487c656cc59c19705
biancaPuscasu <biancapuscasu@yahoo.com>
2017-08-29 11:37:17 +0300

    Constructor

--- DataLayer/Acomodation.cs
+++ DataLayer/Acomodation.cs
@@ -34,7 +34,6 @@ namespace DataLayer
                 return Type.ToString();
             }
         }
-        public Acomodation(AcomodationType Type, string Address,string Name, int NumberOfStars, string AcomodationPhoto, string Description, string PhoneNumber, string WebSite,int CityId,string lat,string lng)
         public string Stars
         {
             get
@@ -63,7 +62,8 @@ namespace DataLayer
                 return stars;
             }
         }
-		{
+        public Acomodation(AcomodationType Type, string Address, string Name, int NumberOfStars, string AcomodationPhoto, string Description, string PhoneNumber, string WebSite, int CityId, string lat, string lng)
+        {
 			this.Type = Type;
 			this.Address = Address;
             this.Name = Name;
