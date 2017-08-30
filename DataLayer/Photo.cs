223f57a16dd99b04b661bf9c32efd2d30ffdf3a4
oanabrisc <oana.brisc29@gmail.com>
2017-08-30 14:11:05 +0300

    database changed

---
+++ DataLayer/Photo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string AcomodationPhoto{ get; set; }
+
+        public int AcomodationId { get; set; }
+
+        public Acomodation Acomodation { get; set; }
+        public Photo()
+        {
+
+        }
+        public Photo(int id,string acomodationPhoto,int acomodationId)
+        {
+            this.Id = id;
+            this.AcomodationPhoto = acomodationPhoto;
+            this.AcomodationId = acomodationId;
+        }
+    }
+}
