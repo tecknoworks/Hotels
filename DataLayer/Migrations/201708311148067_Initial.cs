b666da31dd28245d62301460efed229b4e6b450f
biancaPuscasu <biancapuscasu@yahoo.com>
2017-08-31 14:49:55 +0300

    create initial migration

--- DataLayer/Migrations/201708311148067_Initial.cs
+++ DataLayer/Migrations/201708311148067_Initial.cs
@@ -3,16 +3,14 @@ namespace DataLayer.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class photo : DbMigration
+    public partial class Initial : DbMigration
     {
         public override void Up()
         {
-            DropColumn("dbo.Acomodations", "AcomodationPhoto");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.Acomodations", "AcomodationPhoto", c => c.String());
         }
     }
 }
